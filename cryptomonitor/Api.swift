6f7f919c06920c54bd0a5d4e40e76a58df7f09a9
ahmadsalimi <ahsa9978@gmail.com>
2023-01-13 00:15:19 +0330

    Fix: use en_US locale to format date in API

--- cryptomonitor/Api.swift
+++ cryptomonitor/Api.swift
@@ -21,13 +21,14 @@ class Api {
     
     func loadRange(pair: Pair, from: Date, until: Date) async throws -> [OHLC] {
         let dateformatter = DateFormatter()
+        dateformatter.locale = Locale(identifier: "en_US")
         dateformatter.dateFormat = "yyyy-MM-dd"
         
-        return try await getTimeSeries(pair: pair, otherParams: "start_date=\(dateformatter.string(from: from))&end_date=\(dateformatter.string(from: until.addingTimeInterval(60*60*24)))")
+        return try await getTimeSeries(pair: pair, otherParams: "start_date=\(dateformatter.string(from: from))&end_date=\(dateformatter.string(from: until.addingTimeInterval(60*60*24)))&timezone=\(TimeZone.current.identifier)")
     }
     
     private func getTimeSeries(pair: Pair, otherParams: String) async throws -> [OHLC] {
-        guard let url = URL(string: "\(Api.endpoint)?\(Api.defaultParams)&symbol=\(pair.symbol)&\(otherParams)&apikey=\(config.apiKey)&timezone=\(TimeZone.current.identifier)") else {
+        guard let url = URL(string: "\(Api.endpoint)?\(Api.defaultParams)&symbol=\(pair.symbol)&\(otherParams)&apikey=\(config.apiKey)") else {
             fatalError("Wrong url")
         }
         
